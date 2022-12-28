fecfa104b61dcea6680f085a1f9aa11cc30207c6
YRodrigo-Ruiz <rod_ruiz@hotmail.com.ar>
2022-12-28 15:49:11 -0300

    2da actualizacion register.html

--- DHTravel/app.js
+++ DHTravel/app.js
@@ -5,27 +5,13 @@ const app = express ();
 const path = require("path");           
 
 
-<<<<<<< Rodrigo
 app.listen(3031, () => {
     console.log("servidor a la escucha del puerto 3031");
-=======
-app.listen(3030, () => {
-    console.log("servidor a la escucha del puerto 3030");
->>>>>>> develop
 }); 
 
 const publicPath = path.resolve(__dirname, "./Public");
 app.use( express.static(publicPath) );
 
 app.get("/", (req, res) => {
-<<<<<<< Rodrigo
     res.sendFile(path.join(__dirname, "./Views/register.html"));
 });
-=======
-    res.sendFile(path.join(__dirname, "./Views/index.html"));
-});
-
-app.get('/carrito', function(req, res) {
-    res.sendFile(path.join(__dirname, '/views/carrito.html')); 
-});
->>>>>>> develop
