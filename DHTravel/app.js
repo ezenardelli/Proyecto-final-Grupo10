42fd6cc3f606c6e8cb12651016aacf998189c861
YRodrigo-Ruiz <rod_ruiz@hotmail.com.ar>
2022-12-31 00:57:36 -0300

    maquetado mobile first

--- DHTravel/app.js
+++ DHTravel/app.js
@@ -13,5 +13,9 @@ const publicPath = path.resolve(__dirname, "./Public");
 app.use( express.static(publicPath) );
 
 app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "./Views/index.html"));
+});
+
+app.get("/register", (req, res) => {
     res.sendFile(path.join(__dirname, "./Views/register.html"));
 });
