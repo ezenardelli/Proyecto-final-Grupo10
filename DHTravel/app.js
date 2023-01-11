233bdf9f41ba75f9812ffa2072ac9ec1e8dcbdbc
Eze Nardelli <ezenardelli@gmail.com>
2023-01-11 13:25:45 -0300

    Blanqueo

---
+++ DHTravel/app.js
@@ -0,0 +1,25 @@
+const express = require("express");  
+
+const app = express ();             
+
+const path = require("path");           
+
+
+app.listen(3031, () => {
+    console.log("servidor a la escucha del puerto 3031");
+}); 
+
+const publicPath = path.resolve(__dirname, "./Public");
+app.use( express.static(publicPath) );
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "./Views/index.html"));
+});
+
+app.get("/registro", (req, res) => {
+    res.sendFile(path.join(__dirname, "./Views/register.html"));
+});
+
+app.get("/detalle", (req, res) => {
+    res.sendFile(path.join(__dirname, "./Views/productDetail.html"));
+});
