17c71f48d0bd4fc9723a61befd9e0dec96c05bb7
Laurapederiva <laura.pederiva@mi.unc.edu.ar>
2023-01-11 22:51:51 -0300

    renombre rutas

--- DHTravel/app.js
+++ DHTravel/app.js
@@ -6,12 +6,12 @@ const path = require("path");
 
 
 app.listen(3031, () => {
-    console.log("servidor a la escucha del puerto 3030");
+    console.log("servidor a la escucha del puerto 3031");
 }); 
 
 const publicPath = path.resolve(__dirname, "./Public");
 app.use( express.static(publicPath) );
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "./Views/index.html"));
+    res.sendFile(path.join(__dirname, "./Views/login.html"));
 });
